d0194bd30b27ceb877e461d206c21ec71b3cba84
Ruanqin <teldrassildruid@gmail.com>
2023-02-13 15:27:40 -0500

    set up basic structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.scss';
+
+import About from './containers/About/About';
+import Work from './containers/Work/Work';
+import Header from './containers/Header/Header';
+import Footer from './containers/Footer/Footer';
+import Skills from './containers/Skills/Skills';
+import Testimonial from './containers/Testimonial/Testimonial';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
