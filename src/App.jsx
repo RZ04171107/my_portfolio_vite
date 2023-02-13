25e927fc71f79a05280564eb16f742a4e9518a53
Ruanqin <teldrassildruid@gmail.com>
2023-02-13 17:49:16 -0500

    finish navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <Navbar />
       <Header />
       <About />
