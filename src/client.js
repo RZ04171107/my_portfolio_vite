5d7f33bb00da6f8aa7c8111c33edd5b3bd4b2fc9
Ruanqin <teldrassildruid@gmail.com>
2023-02-14 00:52:25 -0500

    add HOC and modify header and about

--- src/client.js
+++ src/client.js
@@ -2,9 +2,14 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: '',
-  dataset: '',
-  apiVersion: '',
+  projectId: '7bomq032',
+  dataset: 'production',
+  apiVersion: '2023-02-01',
   useCdn: true,
-  toker: '',
+  token:
+    'sk3crueD0Bj62umWU9ROFKAAokMFOmzRw7y4MxxnWCOl8JF8wsq3YRr2COBLt4tu1xW9E7qr1eR92Z8Wfq2OlHAObjfbpKmxw8PpBazl54E1NA2SbT2TT2mUVpuCRRbOYpI4plg3qcmsU6rKdPYn7kP5IwyoT0Bprx3p46yfslgekUoH2G6t',
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import './About.scss';
 import { images } from '../../constants';
+import { urlFor, client } from '../../client';
+import AppWrap from '../../wrapper/AppWrap';
 
 const abouts = [
   {
@@ -28,6 +30,15 @@ const abouts = [
 ];
 
 const About = () => {
+  // const [abouts, setAbouts] = useState([]);
+
+  // useEffect(() => {
+  //   const query = '*[_type == "abouts"]';
+  //   client.fetch(query).then((data) => {
+  //     setAbouts(data);
+  //   });
+  // }, []);
+
   return (
     <>
       <h2 className='head-text'>
@@ -57,4 +68,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default AppWrap(About, 'about');
